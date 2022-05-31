92669ec6010dea4d05802365c78ee86dbdcf55c2
Antonio Calazans <106456914+AntonioCalazansQA@users.noreply.github.com>
2022-05-31 14:55:55 -0300

    Bug Added

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Um QA no multiverso de Devops!"
 
+@app.rout('/bug')
+def bad():
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)
+    except TypeError as e:
+        print("Duplicado, ou seja, nunca vai entrar aqui.")
+
+
 if __name__ == '__main__':
     app.run()
